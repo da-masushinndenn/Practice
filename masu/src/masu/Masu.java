94550c669b22f5d43fe945084ab38a6332050841
da-masushinndenn <daamasushinndenn@gmail.com>
2014-10-02 22:30:01 +0900

    初回コミット

---
+++ masu/src/masu/Masu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package masu;
+
+/**
+ *
+ * @author ダーマス神殿
+ */
+public class Masu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //System.out.println((2048 * (1024 + 1024)) - 8658 +
+        //                             896 * 1024);
+        
+        
+        
+    }
+    
+}
