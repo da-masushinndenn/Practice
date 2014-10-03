7a4975f0bea7687eeb995a9ebcc46c83e28c885e
ダーマス神殿 <daamasushinndenn@gmail.com>
2014-10-03 20:17:11 +0900

    if文　学習

---
+++ masu/src/masu/IfPractice.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package masu;
+
+/**
+ *
+ * @author ダーマス神殿
+ */
+public class IfPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //変数 yuushaExpをint型で宣言。0に初期化
+        int yuushaExp = 0;
+        
+        //もしyushaExpが 1000以上なら
+        if(yuushaExp >= 1000){
+            //レベルアップ！メッセージを表示
+            System.out.println("レベルアップしました！");
+            
+        }
+        
+        System.out.println("全処理終わり");
+    }
+    
+}
