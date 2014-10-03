8cfcd26de8dbeadfd85265a3b13afba558375d7b
da-masushinndenn <daamasushinndenn@gmail.com>
2014-10-04 00:45:07 +0900

    論理演算子　複数条件

--- masu/src/masu/IfPractice.java
+++ masu/src/masu/IfPractice.java
@@ -17,16 +17,32 @@ public class IfPractice {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //変数 yuushaExpをint型で宣言。0に初期化
-        int yuushaExp = 0;
+        //変数 yuushaExpをint型で宣言。750に初期化
+        int yuushaExp = 750;
         
         //もしyushaExpが 1000以上なら
         if(yuushaExp >= 1000){
             //レベルアップ！メッセージを表示
             System.out.println("レベルアップしました！");
             
+        }else if(yuushaExp >= 900){
+            //あと少し！メッセージを表示
+            System.out.println("あと少し！" + "経験値：" + yuushaExp);
+            
+        //もしyuushaExpが700以上800以下ならエクトラレベルアップを表示
+        }else if(yuushaExp >= 700 && yuushaExp <= 800){
+                //特別レベルアップメッセージ表示
+            System.out.println("エクストラレベルアップ");
+        }else if(yuushaExp == 555 || yuushaExp == 666){
+            //呪いにかかった！レベルダウンメッセージを表示
+            System.out.println("呪いにかかった！レベルダウン！");
+            
+        //それ以外なら
+        }else{
+            //レベルアップはまだまだ！がんばれ！メッセージを表示
+            System.out.println("もっとがんばれ！" + "経験値：" + yuushaExp);
         }
-        
+                   
         System.out.println("全処理終わり");
     }
     
