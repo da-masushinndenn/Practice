e389c5b576eb908457ff01856eabd85628855db0
da-masushinndenn <daamasushinndenn@gmail.com>
2014-10-28 22:38:28 +0900

    errortest

---
+++ KotoDebug/src/kotodebug/KDMain.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kotodebug;
+
+/**
+ *
+ * @author ダーマス神殿
+ */
+public class KDMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //実行時エラーテスト
+        System.out.println("");
+        System.out.println("********** 実行時エラーテスト結果 **********");
+        System.out.println("");
+        
+        int a = 3; //変数aを3で宣言して初期化
+        System.out.println("100を" + a + "で割ると" + ( 100/a ) + "です");
+        
+        a -= 1;
+        System.out.println("100を" + a + "で割ると" + (100/a) + "です");
+        
+        //********************
+        //論理エラーテスト
+        System.out.println("");
+        System.out.println("********** 論理エラーテスト結果 **********");
+        System.out.println("");
+        
+        int b = 100;
+        System.out.println("100を" + b + "で割ると" + (100/a) + "です");
+    }
+    
+}
--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,86 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package KQ;
+package test;
 
 /**
  *
  * @author ダーマス神殿
  */
-public class KQMain {
-        
-    //*****************************************
-    //定数
-    //*****************************************
-    //入力・表示処理モード
-    private static final int EXEC_MODE_QUIZ = 1; //問題処理モード
-    private static final int EXEC_MODE_CHECK = 2; //回答処理モード
-    private static final int QUIZ_MAX = 4; //問題数
-
-    //*****************************************
-    //変数
-    //*****************************************
-    private static boolean loopFlg = true;//ループフラグ
-    private static int inputNum = 0; //入力番号
-    private static int execMode = EXEC_MODE_QUIZ; //処理モード（問題処理モードで開始）
-
-    private static int nowQuizNum = 0; //現在出題番号
-    private static int correctTotal = 0; //正解数
+public class Test {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
-        //*****************************************
-        //メイン処理
-        //*****************************************
-        while (loopFlg) {
-            ///////////////////////////////////////
-            //実行と描画処理
-
-            //問題処理モード
-            if (execMode == EXEC_MODE_QUIZ) {
-                
-                //問題処理モード関数呼び出し
-                execModeQuiz();
-
-
-                //解答処理モード
-            } else if (execMode == EXEC_MODE_CHECK) {
-
-                //解答処理モード関数呼び出し
-                exeModeCheck();
-
-            }
-
-
-            
-            //入力番号を受け取る
-            inputNum = ScanInputData();
-            
-            //表示終了の区切り
-            System.out.println("");
-            System.out.println("######################################################");
-            System.out.println("");
-
-        }
-
-        //結果表示
-        System.out.println("**************** ことくいず　********************");
-        System.out.println("");
-        System.out.println("　　　　　　あなたの正解数は" + correctTotal + "でした。");
-        System.out.println("************************************************");
-        System.out.println("");
-        System.out.println("おしまい");
-
     }
-
-    /**
-     * 問題処理モード
-     */
-    private static void execModeQuiz(){
+    
+        private static void execModeQuiz(){
         //テスト用：見出し表示
         System.out.println("*********** ことくいず　***********");
         System.out.println("        現在、" + (nowQuizNum + 1) + "問目です。");
@@ -116,11 +52,6 @@ public class KQMain {
                 System.out.println("3：(a >= 4 && a <= 10) || a < 40)");
 
                 break;
-            case 3:
-                System.out.println("ことが一番好きな場所はどこ？");
-                System.out.println("");
-                System.out.println("-----------------------------------------");
-                System.out.println("1:動物園 2:水族館 3:遊園地");
         }
         System.out.println("答え＞");
 
@@ -149,10 +80,6 @@ public class KQMain {
                 correctTotal = checkAnswerData(3, inputNum, correctTotal);
 
                 break;
-            case 3:
-                //正解番号チェック
-                correctTotal = checkAnswerData(2, inputNum, correctTotal);
-                
         }
         System.out.println("");
         System.out.println("-------------------------------------------");
@@ -196,26 +123,4 @@ public class KQMain {
         return seikaiGoukei;
     }
     
-        private static int ScanInputData(){
-                    //キー入力処理
-        int tmpInputNum = 0; //入力番号初期化
-        try {
-            //初期化処理
-            final int IMPUT_MAX = 3; //最大入力値
-            //キー入力読み込み処理（int型）
-            java.util.Scanner sc = new java.util.Scanner(System.in);
-            int inputInt = sc.nextInt();
-            //入力値チェックと入力番号への代入
-            if (inputInt > 0 && inputInt <= IMPUT_MAX) {
-                tmpInputNum = inputInt;
-            } else {
-                System.out.println("※ コマンドは" + IMPUT_MAX + "以下で入力して下さい ※ ");
-            }
-        } catch (Exception e) {
-            System.out.println("※ 数字以外は入力しないで下さい ※");
-        }
-        return tmpInputNum;
-            //キー入力処理　ここまで
-    }
-
 }
